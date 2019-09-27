6711305b7867f947f5170a7a10b2748c2da45d7f
Emi Rezaei <tomoyose.emi@gmail.com>
2019-09-26 20:33:05 -0700

    wip-added more styleing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let time = moment().format('HH'); 
+
 
 
 //create 1 row div with 3 columns.
@@ -14,19 +16,30 @@ for(let i = 9; i <= 17; i++){
     else{
         column1.text(i-12 + " PM")
     }
-    column1.attr("class", "col-md-2 hour");
-    column1.attr("data-id", i);
-    let column2 = $("<input>");
-    let object = JSON.parse(localStorage.getItem(i));
-    if(object){
-        column2.val(object.todok);
-    }
     
-    column2.attr("class", "col-md-8");
-    column2.attr("data-id", i);
+    column1.attr("class", "col-md-2 hour");
+    column1.attr("data-row", i);
+    let column2 = $("<input>");
+    let inputVal = JSON.parse(localStorage.getItem(i));
+    if(inputVal){
+        column2.val(inputVal.todok);
+    }
+
+    // column2.attr("class", "col-md-8");
+    column2.attr("data-row", i);
+    if(i < time){
+        column2.attr("class", "col-md-8 past");
+    }
+    else if(i === time){
+        column2.attr("class", "col-md-8 present");
+    }
+    else{
+        column2.attr("class", "col-md-8 future")
+    }console.log(column2);
+
     let column3 = $("<button>");
-    column3.attr("class", "col-md-2 saveBtn");
-    column3.attr("data-id", i);
+    column3.attr("class", "col-md-2 saveBtn fas fa-piggy-bank fa-3x");
+    column3.attr("data-row", i);
     rowDiv.append(column1);
     rowDiv.append(column2);
     rowDiv.append(column3);
@@ -37,8 +50,8 @@ for(let i = 9; i <= 17; i++){
 
 $("button").on("click",function(event){
     event.preventDefault();
-    let data_id = $(this).data("id");
-    let rowEls = $('[data-id="'+data_id+'"]');
+    let data_id = $(this).data("row");
+    let rowEls = $('[data-row ="'+data_id+'"]');
     let todo = rowEls[1].value;
     let time = rowEls[0].textContent;
     let saveInput = {
